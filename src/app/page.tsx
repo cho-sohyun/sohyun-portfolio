0a4f0abaab7d4fcc228099e4b614789dea068b4d
cho-sohyun <shteam1107@gmail.com>
2025-10-01 01:29:02 +0900

    refactor: 섹션 구조 단순화 및 불필요한 래퍼 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,35 +9,18 @@ import Footer from "./components/Footer";
 import BlogPage from "./sections/BlogPage";
 
 // section padding 통일 필요
-// home에서 section 대체 고민
 // scroll top 버튼 추가 예정
 
 export default function Home() {
   return (
     <main className="relative w-full">
-      <section className="w-full h-screen">
-        <Intro />
-      </section>
-
-      <section>
-        <About />
-      </section>
-      <section>
-        <SkillPage />
-      </section>
-      <section>
-        <CareerPage />
-      </section>
-      <section>
-        <ProjectPage />
-      </section>
-      <section>
-        <BlogPage />
-      </section>
-
-      <section>
-        <Footer />
-      </section>
+      <Intro />
+      <About />
+      <SkillPage />
+      <CareerPage />
+      <ProjectPage />
+      <BlogPage />
+      <Footer />
     </main>
   );
 }
