bc60b7b2da5c320c67f30dfbd5e3a561a25a4c31
cho-sohyun <shteam1107@gmail.com>
2025-10-07 22:39:03 +0900

    chore: favicon 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,13 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ScrollTopButton from "./components/ScrollTopButton";
 
+export const metadata = {
+  title: "sohyun portfolio",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
