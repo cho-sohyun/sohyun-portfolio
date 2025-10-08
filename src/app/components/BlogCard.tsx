1958b8ca663ff1db574ea1e3237d8c68a015f075
cho-sohyun <shteam1107@gmail.com>
2025-10-08 17:35:49 +0900

    feat: BlogCard 컴포넌트 모바일 오버레이 추가

--- src/app/components/BlogCard.tsx
+++ src/app/components/BlogCard.tsx
@@ -1,12 +1,25 @@
 "use client";
 import Image from "next/image";
+
 import { BlogData } from "@/constants/blog";
+import { useState } from "react";
 
 type BlogCardProps = BlogData;
 
 const BlogCard = ({ title, description, image, link }: BlogCardProps) => {
+  const [showOverlay, setShowOverlay] = useState(false);
+
+  const handleToggle = () => {
+    if (window.innerWidth < 768) {
+      setShowOverlay((prev) => !prev);
+    }
+  };
+
   return (
-    <div className="relative group w-full sm:w-1/2 lg:w-1/3 p-4">
+    <div
+      className="relative group w-full sm:w-1/2 lg:w-1/3 p-4"
+      onClick={handleToggle}
+    >
       <Image
         src={image}
         alt={title}
@@ -17,16 +30,19 @@ const BlogCard = ({ title, description, image, link }: BlogCardProps) => {
       <div
         className={`
       absolute inset-0 bg-black transition-opacity duration-300 flex flex-col justify-start p-4 
-      opacity-0 group-hover:opacity-80 
-      md:opacity-0 md:group-hover:opacity-80
-      sm:opacity-100 sm:group-hover:opacity-100
-    `}
+      ${
+        showOverlay ? "opacity-80" : "opacity-0"
+      } md:opacity-0 md:group-hover:opacity-80`}
       >
         <p className="text-white text-start p-2 custom-font text-lg">{title}</p>
         <p className="text-white text-start p-2 font-normal text-sm">
           {description}
         </p>
-        <div className="absolute bottom-4 left-4 flex opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <div
+          className={`absolute bottom-4 left-4 flex transition-opacity duration-300 
+          ${showOverlay ? "opacity-100" : "opacity-0"} 
+          md:opacity-0 md:group-hover:opacity-100`}
+        >
           <a
             href={link}
             target="_blank"
